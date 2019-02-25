67f602751a43e2503cb94cd02297bc962b0fd38a
Thaynan Andrey <thaynan.nunes@ccc.ufcg.edu.br>
2019-02-25 10:57:38 -0300

    Adicionada navbar e página inicial da aplicação

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="dashboard container">
+        <div className="row">
+          <h1>
+            Dashboard
+          </h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
